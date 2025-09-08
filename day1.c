d8ab8db11d27f4876a878cd7934c4bdc2ac78e13
Muzamil Ali <muzamil.workdev@gmail.com>
2025-09-08 16:47:23 +0500

    first-day

--- day1.c
+++ day1.c
@@ -6,19 +6,19 @@ int main(){ //driver code
 // double precision : double 8 byte 64 bits 2.3E−308 to 1.7E+308
 // character : char 1 byte 8 bits −128 to 127
 
-int var  = 65;
-float ar1 = 99999.7;
-double ba2 = 100000.55;
-char zee= 'f';
+// int var  = 65;
+// float ar1 = 99999.7;
+// double ba2 = 100000.55;
+// char zee= 'f';
 // collection of character is called string "" <- string '' <- character
 
-char a = 's';
-char b = 'h';
-char c = 'a';
-char d = 'h';  
-char e = 'm';
-char f = 'i';
-char g = 'r';
+// char a = 's';
+// char b = 'h';
+// char c = 'a';
+// char d = 'h';  
+// char e = 'm';
+// char f = 'i';
+// char g = 'r';
 // printf("%c",a);
 // printf("%c",b);
 // printf("%c",c);
@@ -51,6 +51,28 @@ char g = 'r';
 // printf("%i ",count);
 // count = 3;
 // printf("%i  ",count);
+
+// Arithematic operations + - / * %
+
+// int a = 0;
+// a = 67.3; //variable means vary able
+
+// int a = 2.2;
+// int b = 1;
+// int c = a+b;
+// int d = a*b;
+// int e =a/b;
+// int f = a-b;
+
+
+// printf("add:%i multi:%i division:%i sub:%i",c,d,e,f);
+// printf("multi: %i",(int)a*b);
+
+int sub1 = 90;
+int sub2 = 80;
+int sub3 =70;
+float d =(sub1+sub2+sub3)/3;
+printf("%f",d);
+
     return 0 ;
 }
-
