5f6af94c9cd962bbe30a99a0045e00bba1c9c595
Muzamil Ali <muzamil.workdev@gmail.com>
2025-09-08 15:48:17 +0500

    first-day

---
+++ day1.c
@@ -0,0 +1,56 @@
+#include<stdio.h> //standard input output header file .... dependencies
+int main(){ //driver code
+
+// integer : int 4 byte 32 bits −2,147,483,648 -> +2,147,483,647 
+// floating point : float 4 byte 32 bits 1.2E−38 to 3.4E+38
+// double precision : double 8 byte 64 bits 2.3E−308 to 1.7E+308
+// character : char 1 byte 8 bits −128 to 127
+
+int var  = 65;
+float ar1 = 99999.7;
+double ba2 = 100000.55;
+char zee= 'f';
+// collection of character is called string "" <- string '' <- character
+
+char a = 's';
+char b = 'h';
+char c = 'a';
+char d = 'h';  
+char e = 'm';
+char f = 'i';
+char g = 'r';
+// printf("%c",a);
+// printf("%c",b);
+// printf("%c",c);
+// printf("%c",d);
+// printf("%c",e);
+// printf("%c",f);
+// printf("%c",g);
+
+// printf("%c%c%c%c%c%c%c",a,b,c,d,e,f,g);
+// printf("shahmir");
+
+// ASCCI FORMAT FOR CONVERSION OF INT TO CHAR ...
+// int A = 65;
+// printf("%c",A);
+
+// char ch = 'B';
+// printf("%i",ch);
+
+//int count = 0; //initiatlization -> declaration + assignment
+//int count;//declaration
+//count = 1; // assignment
+
+//  keyword int char #include computer already knows its meaning
+//  identifier user defined names
+
+// PRACTICE FOR VARIABLE ASSIGNMENT
+// int count = 1;
+// printf("%i ",count);
+// count = 2;
+// printf("%i ",count);
+// count = 3;
+// printf("%i  ",count);
+    return 0 ;
+}
+
