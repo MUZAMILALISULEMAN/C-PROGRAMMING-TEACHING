0975ddb590c89abcbc609746fa608a5984cec996
bharamfahad05 <fahad2201047@gmail.com>
2025-09-10 06:19:25 -0700

    Add files via upload

---
+++ fahad-practice/cuboid.c.c
@@ -0,0 +1,25 @@
+#include<stdio.h>;
+int main(){
+	
+int num2=153;
+int num=153;
+int x=num%10;
+num=num/10;
+int y=num/10;
+int z=num%10;
+int k=(x*x*x)+(y*y*y)+(z*z*z);
+printf("if the answer is 0 then it is cuboid %i",num2-k);	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
