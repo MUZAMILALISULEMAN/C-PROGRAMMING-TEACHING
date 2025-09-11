ea914391a87c5359a6c30252b7f8a1f046df102e
Muzamil Ali <muzamil.workdev@gmail.com>
2025-09-11 16:41:09 +0500

    added

---
+++ day4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    //print => 341;
+    
+    // int number = 1234;
+    // int x = number%10;
+    // number = number/10;
+    // int y = number%10;
+    // number = number/10;
+    // int z = number%10;
+    // number = number/10;
+    // int t = number%10;
+
+
+    // printf("%i",x);
+    // printf("%i",y);
+    // printf("%i",z);
+
+//     int a = x*1000;
+//     int b = y*100;
+//     int c = z*10;
+//     int d = t;
+//     int k = a+b+c+d;
+// printf("%i",k);
+
+
+
+
+
+return 0;
+}
